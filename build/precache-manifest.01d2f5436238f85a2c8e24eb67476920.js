90e92f3827475f765960485db606e1c815675485
Alex Wolf <alexsearchwolf@gmail.com>
2020-03-11 23:21:42 +0100

    commit

---
+++ build/precache-manifest.01d2f5436238f85a2c8e24eb67476920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118d4da40be2eb0a36b53f57b2778ff3",
+    "url": "/OnlineMarket_react.git/index.html"
+  },
+  {
+    "revision": "10423c351fb2dd22045b",
+    "url": "/OnlineMarket_react.git/static/css/main.c42b23d9.chunk.css"
+  },
+  {
+    "revision": "d91ac1247d68c3173e0b",
+    "url": "/OnlineMarket_react.git/static/js/2.29ba3a2c.chunk.js"
+  },
+  {
+    "revision": "10423c351fb2dd22045b",
+    "url": "/OnlineMarket_react.git/static/js/main.70956428.chunk.js"
+  },
+  {
+    "revision": "7f3d9e328ce74320f84d",
+    "url": "/OnlineMarket_react.git/static/js/runtime-main.93a93a78.js"
+  },
+  {
+    "revision": "0491848256221a318aa90d0115986f95",
+    "url": "/OnlineMarket_react.git/static/media/image.04918482.jpg"
+  }
+]);
